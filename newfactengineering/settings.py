8a27792d8e805f88d4a21ab5a5c04bd91c59f890
smiley dinesh <smileydinesheee@gmail.com>
2026-05-17 01:27:47 +0530

    updated allowed hostss

--- newfactengineering/settings.py
+++ newfactengineering/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-w@_gpm$z%drd#g-^@v894j(6a8ov5_8m)kcjoag5!+%##2nnb^'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
